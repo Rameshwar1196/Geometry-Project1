762cac1563be80dd16bb537999ea068209801297
hkr1 <kalbhorharidas@gmail.com>
2021-04-17 09:44:02 +0530

    Stl file reading.
    Stl file reading.

--- point3d.cpp
+++ point3d.cpp
@@ -1,5 +1,21 @@
 #include "point3d.h"
+point3d::point3d(double xx, double yy, double zz)
+{
+	x = xx;
+	y = yy;
+	z = zz;
+}
+
+triangle::triangle(
+	const point3d& p1,
+	const point3d& p2,
+	const point3d& p3)
+{
+	v1 = p1;
+	v2 = p2;
+	v3 = p3;
+}
 point3d triangle::closest_point(point3d pointiputpt)
 {
-
+	return point3d();
 }
